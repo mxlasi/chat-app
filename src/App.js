5e92119189b4c95f3274db679464872650acd780
Laura Sillanpää <57991562+mxlasi@users.noreply.github.com>
2020-04-28 13:00:52 +0300

    messages stored into database!

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import React from 'react';
 import './App.css';
 import Chatbox from './components/Chatbox';
 import {Link} from 'react-router-dom';
+import firebase from './firebase';
 
 class App extends React.Component {
 
 constructor(props) {
   super(props);
   this.state = {
-    term: '',
-    items: []
+    message: ''
      };
   }
 
 onChange = (event) => {
-  this.setState({ term: event.target.value });
+  this.setState({[event.target.name]: event.target.value });
 }
 
 onSubmit = (event) => {
-  event.preventDefault()
-  this.setState({
-  term: '',
-items: [...this.state.items, this.state.term]
-});
+  event.preventDefault();
+  if(this.state.message !== ''){
+    const chatRef = firebase.database().ref('general');
+    const chat = {
+      message: this.state.message,
+      user: this.props.user.displayName,
+      timestamp: new Date().getTime()
+    }
+
+    chatRef.push(chat);
+    this.setState({message: ''});
+
+  }
 }
 
 render() {
@@ -32,9 +40,16 @@ render() {
 
       {this.props.user &&
       <div className="allow-chat">
-      <Chatbox items={this.state.items} />
+      <Chatbox />
+      
       <form className="message-form" onSubmit={this.onSubmit}>
-      <input value={this.state.term} onChange={this.onChange} />
+      <input 
+      type="text"
+      name="message"
+      id="message"
+      value={this.state.message}
+      placeholder="Enter a message..."
+      onChange={this.onChange} />
 <button>Send</button>
       </form>
     </div>
